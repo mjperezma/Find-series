b353bb3531f7f806407a89e3894c374163a118f2
mj0se <mariajose1907@gmail.com>
2020-09-25 11:29:07 +0200

    review fetch

--- js/main.js
+++ js/main.js
@@ -12,22 +12,22 @@ let favouriteShows = [];
 function getInfoApiShow(ev) {
   ev.preventDefault();
   const inputTextValue = inputText.value;
-  fetch(`http://api.tvmaze.com/search/shows?q=${inputTextValue}`)
-    .then(response => response.json())
-    .then(data => {
+  fetch(`https://api.tvmaze.com/search/shows?q=${inputTextValue}`)
+    .then((response) => response.json())
+    .then((data) => {
       saveShow.splice(0);
       for (let index = 0; index < data.length; index++) {
         if (data[index].show.image === null) {
           saveShow.push({
             name: data[index].show.name,
             id: data[index].show.id,
-            image: 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
+            image: 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV',
           });
         } else {
           saveShow.push({
             name: data[index].show.name,
             image: data[index].show.image.medium,
-            id: data[index].show.id
+            id: data[index].show.id,
           });
         }
       }
@@ -92,7 +92,7 @@ function addFavouriteShow(ev) {
         favouriteShows.push({
           id: foundFavouriteShow.id,
           name: foundFavouriteShow.name,
-          image: foundFavouriteShow.image
+          image: foundFavouriteShow.image,
         });
       }
     }
